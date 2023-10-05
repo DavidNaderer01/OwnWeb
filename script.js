09cca0bb8f08b61ad04fb2892bc248f1235aaa52
Daffie989989 <126651209+DavidNaderer01@users.noreply.github.com>
2023-10-05 22:06:35 +0200

    Add keydown

--- script.js
+++ script.js
@@ -1,15 +1,4 @@
 
-
-var person = {
-    name: "David Naderer",
-    age: 16,
-    classRoom: 154,
-    info:function () {
-        return document.onkeydown = function (key) {
-            if (key.key === 'a')
-            {
-                alert(key.key)
-            }
-        }
-    }
+document.onkeydown = (key) => {
+    alert(key);
 }
