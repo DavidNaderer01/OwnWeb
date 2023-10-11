fa4119d08477227d3f2fb509a53fbdee2179dcb4
Daffie989989 <126651209+DavidNaderer01@users.noreply.github.com>
2023-10-11 21:48:36 +0200

    c

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 var date = new Date();
 
 document.getElementById("header").innerHTML = date.getDate() + "." + date.getMonth() + "." + date.getFullYear();
+
+var children = document.getElementById("headersection").children;
+
+var span = document.createElement("span");
+span.innerHTML = "This is a try";
+
+children.item(1).appendChild(span);
