c2b3423e46e81119101131ff098195f1772e3d4e
DavidNaderer01 <dav1.nad007@gmail.com>
2023-12-16 12:42:23 +0100

    update

--- script.js
+++ script.js
@@ -1,10 +1,72 @@
-const date = new Date();
+let styleType = {
+    menuHeigthClosed: "5%",
+    menuHeigthOpen: "15%",
+    marginMenuClosed: "-8rem",
+    marginMenuOpen: "2rem",
+    pageTopClosed: "7%",
+    pageTopOpen: "17%",
+};
+let pages = {
+    pageIndexer: [
+        {
+            name: "Home",
+            path: "./pages/home.html"
+        },
+        {
+            name: "About me",
+            path: "./pages/about-me.html"
+        }
+    ],
+    getPage: async function (name) {
+        let path = "./pages/default.html";
+        await this.pageIndexer.forEach((value, index) => {
+            if(name === value.name)
+                path = value.path;
+        });
+        return path;
+    }
+}
+let menu = document.getElementById('menu');
+let  ul = menu.lastElementChild;
+let li = ul.children;
+let page = document.getElementById('page');
 
-document.getElementById("header").textContent = date.getDate().toString() + "." + date.getMonth().toString() + "." + date.getFullYear().toString();
+for(let indexer = 0; indexer < li.length; indexer++) {
+    li[indexer].addEventListener('click', () => {
+        for(let i = 0; i < li.length; i++) {
+            li[i].classList.remove('selected');
+        }
+        openURL(li[indexer].textContent);
+        li[indexer].classList.add('selected');
+    });
+}
+document.getElementById("menu-checker").addEventListener('click', () => {
+    if(menu.style.height === styleType.menuHeigthClosed) {
+        menu.style.height = styleType.menuHeigthOpen;
+    }
+    else {
+        menu.style.height = styleType.menuHeigthClosed;
+    }
+    if(ul.style.marginTop === styleType.marginMenuClosed) {
+        ul.style.marginTop = styleType.marginMenuOpen;
+    }
+    else {
+        ul.style.marginTop = styleType.marginMenuClosed;
+    }
+    if(page.style.top === styleType.pageTopClosed) {
+        page.style.top = styleType.pageTopOpen;
+    }
+    else {
+        page.style.top = styleType.pageTopClosed;
+    }
+});
+li[0].click();
 
-let children = document.getElementById("header section").children;
-
-let span = document.createElement("span");
-span.innerHTML = "This is a try";
-
-children.item(1).appendChild(span);
+async function openURL(content) {
+    const path = await pages.getPage(content);
+    fetch(path)
+        .then(response => response.text())
+        .then(data => {
+            page.innerHTML = data;
+        });
+}
